2b0660b03199e100295abb92346aec64a6483e72
tulthix <tulthix@gmail.com>
2022-07-06 17:11:29 -0500

    factor out event and mosquitto wrap

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Lib
     ( mainApp
@@ -6,124 +7,102 @@ module Lib
     , mqttMsgToEventCode
     , MqttMsg(..)
     , buttonEventcodePairs
-    , topicizeButtonEventcodePairs
     , mqttMsgToEventCmd
     ) where
 
-import System.IO
+import System.IO (hGetLine, hPutStrLn, Handle)
 
-import Data.Maybe
-import Data.Functor
-import Data.List.Extra
+import System.Time.Extra (sleep)
 
-import qualified Data.Map as M
-
-import Control.Monad
-
-import Data.ByteString.Lazy (ByteString)
+import Control.Monad ( forever, guard )
 
 import System.Process.Typed
+  (
+    createPipe
+  , withProcessTerm_
+  , getExitCode
+  , ExitCode
+  , Process
+  , setStdin
+  , setStdout
+  , setStderr
+  , getStdout
+  , getStdin
+  , closed
+  , shell
+  )
+import Control.Exception (tryJust)
+import System.IO.Error (isEOFError)
 
-defaultMqttBroker = "mqtt-broker"
-defaultInputDevice = "/dev/input/event3"
-defaultAdbConnectCommand = "adb connect nvidia-shield-tv ; adb shell"
-  
-buttonEventcodePairs :: [(String, Int)]
-buttonEventcodePairs = [ ("dpad_up"    , 103)
-                       , ("dpad_down"  , 108)
-                       , ("dpad_left"  , 105)
-                       , ("dpad_right" , 106)
-                       , ("dpad_center", 28 )
-                       , ("back"       , 1  )
-                       , ("home"       , 172)
-                       , ("menu"       , 139)
-                       , ("volume_up"  , 115)
-                       , ("volume_down", 114)
-                       , ("a"          , 304)
-                       , ("b"          , 305)
-                       , ("x"          , 307)
-                       , ("y"          , 308)
-                       , ("l1"         , 310)
-                       , ("r1"         , 311)
-                       , ("rtrigger"   , 312)
-                       , ("ltrigger"   , 313)
-                       , ("select"     , 314)
-                       , ("start"      , 315)]
-  
-makeMqttCmd :: String -> [String] -> String
-makeMqttCmd host topics =
-  "mosquitto_sub -F \"MSG %t %p\" -h " ++ host ++ (topicsToArgs topics)
-  where topicsToArgs :: [String] -> String
-        topicsToArgs topics = concat (map (\t -> " -t " ++ t) topics)
+import MosquittoWrap
+import Event
 
-
-eventCodeToEventCmd :: String -> Int -> String
-eventCodeToEventCmd device eventCode =
-  "sendevent " ++ device ++ " 1 " ++ (show eventCode) ++ " 1 "
-  ++ "; sendevent " ++ device ++ " 0 0 0"
-  ++ "; sendevent " ++ device ++ " 1 " ++ (show eventCode) ++ " 0 "
-  ++ "; sendevent " ++ device ++ " 0 0 0"
-  ++ "\n"
-
-type MqttTopic = String
-type MqttPayload = String
-
-data MqttMsg = MqttMsg MqttTopic MqttPayload deriving (Show, Eq)
-
-parseMqttLine :: String -> Maybe MqttMsg
-parseMqttLine l =
-  msgTopicPayload >>=
-    \(msgId, topic, payload) -> if msgId == "MSG"
-                                then Just $ MqttMsg topic payload
-                                else Nothing
-  where parts = words l
-        msgTopicPayload :: Maybe (String, MqttTopic, MqttPayload)
-        msgTopicPayload = do
-          msgId <- parts !? 0
-          topic <- parts !? 1
-          payloadStart <- parts !? 2  -- require some payload
-          let payload = unwords $ [payloadStart] ++ (drop 3) parts
-          return (msgId, topic, payload)
-
-mqttMsgToEventCode :: String -> [(String, Int)] -> MqttMsg -> Maybe Int
-mqttMsgToEventCode topicPrefix butEcPairs (MqttMsg topic payload) =
-  if isPayloadOn (lower payload)
-  then (topicEventcodeMap M.!? topic)
-  else Nothing
-  where topicEventcodePairs = topicizeButtonEventcodePairs topicPrefix buttonEventcodePairs
-        topicEventcodeMap = M.fromList topicEventcodePairs
-        isPayloadOn :: MqttPayload -> Bool
-        isPayloadOn "1" = True
-        isPayloadOn "on" = True
-        isPayloadOn "buttonpress" = True
-        isPayloadOn "active" = True
-        isPayloadOn "activate" = True
-        isPayloadOn _ = False
-  
-topicizeButtonEventcodePairs :: String -> [(String, Int)] -> [(String, Int)]
-topicizeButtonEventcodePairs topicPrefix buttonEventCodePairs =
-  topicizeButtonEventcodePair <$> buttonEventCodePairs
-  where topicizeButtonEventcodePair :: (String, Int) -> (String, Int)
-        topicizeButtonEventcodePair (t, e) = (topicPrefix ++ t, e)
-        
 mqttMsgToEventCmd :: String -> String -> [(String, Int)] -> MqttMsg -> Maybe String
 mqttMsgToEventCmd device topicPrefix butEcPairs msg =
-  (eventCodeToEventCmd device) <$> (mqttMsgToEventCode topicPrefix butEcPairs msg)
+  eventCodeToEventCmd device <$> mqttMsgToEventCode topicPrefix butEcPairs msg
+
+safeHGetLine :: Handle -> IO (Either () String)
+safeHGetLine h = tryJust (guard . isEOFError) (hGetLine h)
+
+mqttMsgAction :: Handle -> MqttMsg -> IO ()
+mqttMsgAction eventOutputHandle msg =
+  case eventCmdMaybe of
+    Nothing       -> putStrLn $ "Unable to convert mqtt msg to event command: " ++ show msg
+    Just eventCmd -> do
+      putStrLn $ "Executing command: " ++ eventCmd
+      hPutStrLn eventOutputHandle eventCmd
+  where
+    topicPrefix = "home/downstairs/shield/button/"
+    mqttMsgToEventCmd' = mqttMsgToEventCmd defaultInputDevice topicPrefix buttonEventcodePairs
+    eventCmdMaybe = mqttMsgToEventCmd' msg
+
+
+lineAction :: Handle -> String -> IO ()
+lineAction eventOutputHandle l =
+  maybe
+    (putStrLn $ "Failed Parsing line: " ++ l) -- on parse error
+    mqttMsgAction'           -- on success
+    (parseMqttLine l)                      -- parse the line produce a maybe
+    where mqttMsgAction' = mqttMsgAction eventOutputHandle
+
+lineLoop :: Handle -> Handle -> Handle -> IO ()
+lineLoop mosquittoReadHandle adbWriteHandle adbReadHandle =
+  safeHGetLine mosquittoReadHandle >>=
+  (\case
+      Left err -> print err
+      Right l -> lineAction' l >> lineLoop mosquittoReadHandle adbWriteHandle adbReadHandle
+  )
+  where lineAction' = lineAction adbWriteHandle
+
+myGetExitCode :: Process stdin stdout stderr -> IO (Maybe ExitCode)
+myGetExitCode = getExitCode
 
 mainApp :: IO ()
-mainApp = do
-  let buttons = (fst <$> buttonEventcodePairs)
-  let buttonTopics = (\b -> topicPrefix ++ b) <$> buttons
-  let topics = buttonTopics
-  let procCfg = setStdin createPipe
-                $ setStdout createPipe
-                $ setStderr closed
-                $ shell (makeMqttCmd defaultMqttBroker topics)
-  withProcessTerm_ procCfg $ \p -> do
-    forever $ hGetLine (getStdout p) >>=
-      (\l -> print $ mqttMsgToEventCmd' =<< parseMqttLine l)
-                                            
-
-  putStrLn "Lost connection to subprocess"
-  where topicPrefix = "home/downstairs/shield/button/"
-        mqttMsgToEventCmd' = mqttMsgToEventCmd defaultInputDevice topicPrefix buttonEventcodePairs
+mainApp = forever $ do
+  putStrLn $ "connecting to mosquitto with command: " ++ mosquittoCmd
+  putStrLn $ "connecting to adb shell with command: " ++ adbConnectCommand
+  withProcessTerm_ mosquittoProcCfg $ \mosquittoProc -> do
+    withProcessTerm_ adbProcCfg $ \adbProc -> do
+      sleep 1
+      putStrLn "HERE0"
+      mec <- myGetExitCode adbProc
+      print mec
+      putStrLn "HERE1"
+      lineLoop (getStdout mosquittoProc) (getStdin adbProc) (getStdout adbProc)
+      putStrLn "HERE2"
+    putStrLn "HERE3"
+  putStrLn "Lost connection to mosquitto_sub subprocess.  Sleeping..."
+  sleep 10
+  where buttons = fst <$> buttonEventcodePairs
+        topicPrefix = "home/downstairs/shield/button/"
+        topics = (topicPrefix ++) <$> buttons
+        mosquittoCmd = makeMqttCmd defaultMqttBroker topics
+        adbConnectCommand = defaultAdbConnectCommand
+        mosquittoProcCfg = setStdin createPipe
+                  $ setStdout createPipe
+                  $ setStderr closed
+                  $ shell mosquittoCmd
+        adbProcCfg = setStdin createPipe
+                  $ setStdout createPipe
+                  $ setStderr closed
+                  $ shell adbConnectCommand
---
+++ src/MosquittoWrap.hs
@@ -0,0 +1,67 @@
+module MosquittoWrap
+  (
+    parseMqttLine
+  , mqttMsgToEventCode
+  , defaultMqttBroker
+  , makeMqttCmd
+  , MqttMsg(..)
+  , MqttTopic
+  , MqttPayload
+  ) where
+  
+import Data.List.Extra
+  (
+    lower
+  , (!?)
+  )
+
+import qualified Data.Map as M
+
+type MqttTopic = String
+type MqttPayload = String
+data MqttMsg = MqttMsg MqttTopic MqttPayload deriving (Show, Eq)
+
+defaultMqttBroker = "mqtt-broker"
+
+parseMqttLine :: String -> Maybe MqttMsg
+parseMqttLine l =
+  msgTopicPayload >>=
+    \(msgId, topic, payload) -> if msgId == "MSG"
+                                then Just $ MqttMsg topic payload
+                                else Nothing
+  where parts = words l
+        msgTopicPayload :: Maybe (String, MqttTopic, MqttPayload)
+        msgTopicPayload = do
+          msgId <- parts !? 0
+          topic <- parts !? 1
+          payloadStart <- parts !? 2  -- require some payload
+          let payload = unwords $ payloadStart : drop 3 parts
+          return (msgId, topic, payload)
+
+mqttMsgToEventCode :: String -> [(String, Int)] -> MqttMsg -> Maybe Int
+mqttMsgToEventCode topicPrefix buttonEventcodePairs (MqttMsg topic payload) =
+  if isPayloadOn (lower payload)
+  then topicEventcodeMap M.!? topic
+  else Nothing
+  where topicEventcodePairs = topicizeButtonEventcodePairs topicPrefix buttonEventcodePairs
+        topicEventcodeMap = M.fromList topicEventcodePairs
+        isPayloadOn :: MqttPayload -> Bool
+        isPayloadOn "1" = True
+        isPayloadOn "on" = True
+        isPayloadOn "buttonpress" = True
+        isPayloadOn "active" = True
+        isPayloadOn "activate" = True
+        isPayloadOn _ = False
+
+topicizeButtonEventcodePairs :: String -> [(String, Int)] -> [(String, Int)]
+topicizeButtonEventcodePairs topicPrefix buttonEventCodePairs =
+  topicizeButtonEventcodePair <$> buttonEventCodePairs
+  where topicizeButtonEventcodePair :: (String, Int) -> (String, Int)
+        topicizeButtonEventcodePair (t, e) = (topicPrefix ++ t, e)
+
+makeMqttCmd :: String -> [String] -> String
+makeMqttCmd host topics =
+  "mosquitto_sub -F \"MSG %t %p\" -h " ++ host ++ topicsToArgs topics
+  where topicsToArgs :: [String] -> String
+        topicsToArgs = concatMap (" -t " ++) 
+
